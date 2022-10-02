36f758e9ec690253b03745232aa3548f024af93f
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-10-03 01:40:47 +0300

    about-page.php

---
+++ app/resource/views/pages/about-page.php
@@ -0,0 +1,5 @@
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab beatae doloribus id laborum necessitatibus non qui
+    reprehenderit voluptatibus? Accusantium assumenda consequuntur dolore et inventore ipsam, quis saepe soluta.
+    Corporis dicta enim error harum reprehenderit? Earum exercitationem facere magnam nisi reprehenderit saepe totam.
+    Alias aliquam amet autem corporis dolores error esse ex id labore natus optio pariatur quibusdam sint, voluptates
+    voluptatibus.</p>
