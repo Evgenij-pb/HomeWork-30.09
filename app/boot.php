3bba2b26fc0daf0a0fcd032ab3da8e4796c7b604
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-10-02 22:22:02 +0300

    boot.php

---
+++ app/boot.php
@@ -0,0 +1,12 @@
+<?php
+
+spl_autoload_register(function($className){
+    $classFile = 'app'. DIRECTORY_SEPARATOR . str_replace("\\", DIRECTORY_SEPARATOR, $className) . '.php';
+    if(file_exists($classFile)){
+        include_once $classFile;
+        return true;
+    }
+    return false;
+});
+
+include_once 'app/config.php';
