22c7a1d21743aee0c11b9216be0d5f9d9dace819
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-10-03 01:40:08 +0300

    contacts-page.php

---
+++ app/resource/views/pages/contacts-page.php
@@ -0,0 +1,13 @@
+<div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus ad autem commodi consequatur corporis culpa
+        cumque cupiditate doloremque dolorum eius eligendi enim esse eum excepturi expedita fugit illum impedit itaque
+        maxime nam nesciunt quam quod repellendus sint sunt, totam vel? Ipsum iure modi quaerat repellendus, sunt
+        temporibus unde vero.</p>
+    <ul>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor.</li>
+    </ul>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d8884.817699813708!2d34.604062808055204!3d48.511843431841534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1664750060461!5m2!1sru!2sua" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
