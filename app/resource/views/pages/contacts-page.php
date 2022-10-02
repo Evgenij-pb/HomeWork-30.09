0483cb20c1922bbb4ff2405ce2fb6c0fb0e2ff7b
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-10-03 01:47:16 +0300

    edit contacts-page.php

--- app/resource/views/pages/contacts-page.php
+++ app/resource/views/pages/contacts-page.php
@@ -9,5 +9,5 @@
         <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
         <li>Lorem ipsum dolor.</li>
     </ul>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d8884.817699813708!2d34.604062808055204!3d48.511843431841534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1664750060461!5m2!1sru!2sua" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d8884.817699813708!2d34.604062808055204!3d48.511843431841534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1664750060461!5m2!1sru!2sua" width="550" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
