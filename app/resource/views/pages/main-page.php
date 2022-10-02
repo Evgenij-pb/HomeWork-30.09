a5d3af786605a8caa7729a079b1508f3f0f8574e
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-10-03 01:39:34 +0300

    main-page.php

---
+++ app/resource/views/pages/main-page.php
@@ -0,0 +1,15 @@
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor facere magnam nam, quasi quos veritatis. Aliquam
+    commodi cum cumque dolorem doloribus eius expedita facilis inventore ipsum iure laboriosam molestiae mollitia neque,
+    nihil nobis odio quae ratione recusandae rem repudiandae saepe sunt vitae voluptates. Exercitationem fugiat nemo
+    perferendis, provident rem veniam?</p><p>Illo ipsam nisi perspiciatis repudiandae sapiente. Aut delectus dicta
+    distinctio enim exercitationem facere harum molestias mollitia nostrum nulla, obcaecati odio perferendis porro qui
+    quis quo repellendus? Accusamus adipisci autem doloremque dolores esse facere harum illo in incidunt ipsam
+    laboriosam, libero magnam minima natus, officia omnis quam quo quod quos vero.</p><p>Consectetur cupiditate
+    distinctio facilis optio quod reprehenderit. Aperiam consectetur corporis cumque deserunt doloribus eveniet,
+    mollitia quidem. Alias commodi delectus molestias mollitia similique. A animi aspernatur commodi consequuntur culpa
+    debitis ducimus eveniet exercitationem explicabo ipsa modi, nemo nisi numquam odit omnis perspiciatis placeat quas
+    reiciendis repellat repellendus rerum saepe temporibus velit.</p><p>Accusantium at blanditiis deleniti deserunt
+    dicta dolor dolores eaque earum enim est et ex fugit hic ipsa laborum laudantium minus mollitia nemo nostrum
+    officiis placeat quas quibusdam quidem, quis recusandae repellendus reprehenderit sequi similique temporibus tenetur
+    ullam veritatis vero voluptatibus. Aliquam deleniti est, eum exercitationem laboriosam magnam quisquam repellendus
+    repudiandae!</p>
